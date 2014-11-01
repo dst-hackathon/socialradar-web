c2db4d826318333e1485492288327af58481ac12
Gift <kantikagift@gmail.com>
2014-11-01 14:53:30 +0700

    Change design of question page

--- web/question_list.dart
+++ web/question_list.dart
@@ -1,13 +1,10 @@
 import 'package:polymer/polymer.dart';
 import 'dart:html' show Event, Node, HttpRequest;
 import 'dart:convert' show JSON;
-import 'dart:async' show Future;
 import 'question.dart' show Question;
 import 'category.dart' show Category;
 import 'option.dart' show Option;
 import 'package:core_elements/core_item.dart' show CoreItem;
-import 'package:paper_elements/paper_item.dart' show PaperItem;
-import 'package:paper_elements/paper_tab.dart' show PaperTab;
 
 @CustomTag('question-list')
 class QuestionList extends PolymerElement {
@@ -24,6 +21,7 @@ class QuestionList extends PolymerElement {
     //retrieveQuestions();
     String json = '[{"id": "1", "order": "1", "text": "What are your favorite menus?", "tag": "food"},{"id": "2", "order": "2", "text": "What sports do you play?", "tag": "sport"},{"id": "3", "order": "3", "tag": "movie", "text": "What are your favorite movies?"},{"id": "4", "order": "4", "tag": "music", "text": "What do music genres belong to you?"},{"id": "5", "order": "5", "tag": "book", "text": "What are your favorite books?"}]';
     parseQuestion(json);
+    questionLabel = questions[0].text;
   }
   
   retrieveQuestions() {
@@ -44,27 +42,8 @@ class QuestionList extends PolymerElement {
     print("Question clicked");
     
     String selectedQuestion = target.attributes["value"];
+    questionLabel = target.attributes["text"];
     retrieveCategoryList(selectedQuestion);
-  }
-   
-  questionMouseOver(Event e, var detail, Node target) {
-    clearCategoryName(target.parent.children);
-    
-    PaperItem paperItem = (target as PaperTab).children.first;
-    paperItem.label = paperItem.attributes["text"];
-  }
-  
-  questionMouseOut(Event e, var detail, Node target) {
-    clearCategoryName(target.parent.children);
-  }
-  
-  clearCategoryName(var paperTabs) {
-    for(var item in paperTabs) {
-      if(item is PaperTab) {
-        PaperTab paperTab = item as PaperTab;
-        (paperTab.children.first as PaperItem).label = '';
-      }
-    }
   }
   
   retrieveCategoryList(String questionId) {
