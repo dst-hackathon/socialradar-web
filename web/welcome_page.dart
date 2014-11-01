05186e3e29df97b66119ee6cec1ab3dc7f6e9aa8
Mahatthana Nomsawadi <MNomsawadi@dstsystems.com>
2014-11-01 17:54:09 +0700

    make empty file for runnign page

---
+++ web/welcome_page.dart
@@ -0,0 +1,10 @@
+import 'package:polymer/polymer.dart';
+import 'dart:html';
+
+@CustomTag('welcome-page')
+class WelcomePage extends PolymerElement {
+  
+  WelcomePage.created() : super.created() {
+  }
+  
+}
